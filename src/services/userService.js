d0793ce4aa35a42d931f0b63b676d412047aed20
Kinmilo2k3 <kinmilo2k3@gmail.com>
2025-05-14 13:28:57 +0700

    test

--- src/services/userService.js
+++ src/services/userService.js
@@ -21,7 +21,7 @@ let handleUserLogin = (email, password) => {
                   let isExist = await checkUserEmail(email)
                   if (isExist) {
                         let user = await db.User.findOne({
-                              attributes: ['email', 'roleId', 'password'],
+                              attributes: ['email', 'roleId', 'password', 'firstName', 'lastName'],
                               where: { email: email },
                               raw: true,
 
@@ -113,26 +113,28 @@ let createNewUser = (data) => {
                   if (check === true) {
                         resolve({
                               errCode: 1,
-                              message: 'Your email is already in used, pls try another email'
+                              errMessage: 'Your email is already in used, pls try another email'
+                        })
+                  } else {
+                        let hashPasswordFromBcrypt = await hashUserPassword(data.password);
+                        await db.User.create({
+                              email: data.email,
+                              password: hashPasswordFromBcrypt,
+                              firstName: data.firstName,
+                              lastName: data.lastName,
+                              address: data.address,
+                              phonenumber: data.phonenumber,
+                              gender: data.gender,
+                              roleId: data.roleId,
+                              positionId: data.positionId,
+                              image: data.avatar
+                        })
+
+                        resolve({
+                              errCode: 0,
+                              message: 'ok'
                         })
                   }
-
-                  let hashPasswordFromBcrypt = await hashUserPassword(data.password);
-                  await db.User.create({
-                        email: data.email,
-                        password: hashPasswordFromBcrypt,
-                        firstName: data.firstName,
-                        lastName: data.lastName,
-                        address: data.address,
-                        phonenumber: data.phonenumber,
-                        gender: data.gender === '1' ? true : false,
-                        roleId: data.roleId,
-                  })
-
-                  resolve({
-                        errCode: 0,
-                        message: 'ok'
-                  })
             } catch (e) {
                   reject(e);
             }
@@ -165,7 +167,7 @@ let deleteUser = (userId) => {
 let updateUserData = (data) => {
       return new Promise(async (resolve, reject) => {
             try {
-                  if (!data.id) {
+                  if (!data.id || !data.roleId || !data.positionId || !data.gender) {
                         resolve({
                               errCode: 2,
                               errMessage: " Missing required parameters"
@@ -179,7 +181,13 @@ let updateUserData = (data) => {
                         user.firstName = data.firstName;
                         user.lastName = data.lastName;
                         user.address = data.address;
-
+                        user.roleId = data.roleId;
+                        user.positionId = data.positionId;
+                        user.gender = data.gender;
+                        user.phonenumber = data.phoneNumber;
+                        if (data.avatar) {
+                              user.image = data.avatar;
+                        }
                         await user.save();
 
                         resolve({
@@ -198,12 +206,36 @@ let updateUserData = (data) => {
       })
 }
 
+let getAllCodeService = (typeInput) => {
+      return new Promise(async (resolve, reject) => {
+            try {
+                  if (!typeInput) {
+                        resolve({
+                              errCode: 1,
+                              errMessage: 'Missing required parameters !'
+                        })
+                  } else {
+                        let res = {};
+                        let allcode = await db.Allcode.findAll({
+                              where: { type: typeInput }
+                        });
+                        res.errCode = 0;
+                        res.data = allcode;
+                        resolve(res);
+                  }
+            } catch (e) {
+                  reject(e);
+            }
+      })
+}
+
 module.exports = {
       handleUserLogin: handleUserLogin,
       checkUserEmail: checkUserEmail,
       getAllUsers: getAllUsers,
       createNewUser: createNewUser,
       deleteUser: deleteUser,
-      updateUserData: updateUserData
+      updateUserData: updateUserData,
+      getAllCodeService: getAllCodeService
 
 }      
